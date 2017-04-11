1040385f881ac9653b0f9dfce9d882aaff19594e
rocky.liug <lau_rocky@163.com>
2017-04-11 15:30:57 +0800

    增加部分测试代码

---
+++ src/views/Main.vue
@@ -0,0 +1,17 @@
+
+<template>
+  <div>
+		<h1>Weex 效果演示!</h1>
+		<ul>
+			<li>
+				<router-link to="/slider">图片滑动演示</router-link>
+			</li>
+		</ul>
+		<router-view></router-view>
+  </div>
+</template>
+
+<style scoped>
+</style>
+<script>
+</script>
